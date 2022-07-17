48bed87885a8df3e065df855c8a8abc93ab945b0
Anurag Singh Rajawat <99046684+anurag-asr@users.noreply.github.com>
2022-07-17 19:52:27 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Form from "./Components/Form";
+import LayOut from "./Components/LayOut";
+import "./styles.css";
+import { SimpleGrid } from '@chakra-ui/react'
+import Stack1 from "./Components/Stack";
+
+export default function App() {
+  return (
+    <div>
+      {/* <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2> */}
+    {/* <Form/> */}
+    {/* <LayOut/> */}
+    <Stack1/>
+    </div>
+  );
+}
